1349d2d231cadf846af18718d9cd35a6c9539b21
Valerie Rose <valerie.rose.d@gmail.com>
2017-02-22 17:25:45 -0800

    jg

--- LibSeat/ViewController.swift
+++ LibSeat/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("test - eric is now making a test")
+        print("uyufuft")
     }
 
     override func didReceiveMemoryWarning() {
