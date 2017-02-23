7e258ed04f52c0948a5fc225c7293e6e611f052d
Valerie Rose <valerie.rose.d@gmail.com>
2017-02-22 17:42:59 -0800

    spinn

--- LibSeat/FilterViewController.swift
+++ LibSeat/FilterViewController.swift
@@ -1,25 +1,28 @@
 //
-//  ViewController.swift
+//  File.swift
 //  LibSeat
 //
-//  Created by Eric Cuyle on 2/15/17.
+//  Created by Valerie Rose on 2/22/17.
 //  Copyright © 2017 BUS498. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FilterViewController: UIViewController {
+    
     override func viewDidLoad() {
-        //super.viewDidLoad()
+        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("uyufuft")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var Spinner: UIPickerView!
+    @IBOutlet weak var myLabel: UILabel!
+    let pickerData = [1,2,3,4,5,6,7,8,9,10]
 }
 
